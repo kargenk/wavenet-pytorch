5c30527daa7e440123f4e136a695a36b7fc81931
gengen <gengen0630@gmail.com>
2022-08-11 03:42:56 +0900

    [init] add dependency files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='my-tts',
+    version='0.1.0',
+    description='my library for tts',
+    author='gengen',
+    packages=['utils'],
+)
